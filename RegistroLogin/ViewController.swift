2b45d7850d5416d00c2fc835094c241c0d899419
Miguel Angel Jimenez <miguejm@outlook.es>
2020-03-10 08:37:09 -0600

    Initial Commit

---
+++ RegistroLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegistroLogin
+//
+//  Created by Miguel Angel Jimenez Melendez on 3/10/20.
+//  Copyright © 2020 Miguel Angel Jimenez Melendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
